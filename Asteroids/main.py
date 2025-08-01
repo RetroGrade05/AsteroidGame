48c92a38bd80ecd559a2c0acb3244f3e61786d2c
retrograde <flamingsword1463@gmail.com>
2025-08-01 11:12:10 -0600

    B: Added FPS limiter

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,22 +1,28 @@
 import pygame
 from constants import *
 
-#Test print
-#__________________
+# Main
+
 def main():
+    #starting prints
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    #screen initialization
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clk = pygame.time.Clock()
+    dt = 0
+
+    #game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,"#000000")
         pygame.display.flip()
-
-#__________________
+        dt = (pygame.time.Clock.tick(clk, 60)) / 1000
 
 if __name__ == "__main__":
     main()
