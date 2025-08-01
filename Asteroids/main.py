8df0423d0ce611ebcfdd23c9c5d72847761daded
retrograde <flamingsword1463@gmail.com>
2025-08-01 12:08:43 -0600

    C: Added functional player

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 # Main
 
@@ -13,16 +14,37 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clk = pygame.time.Clock()
+
+    #groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
+
     dt = 0
+    player = Player(x = SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+
+    
 
     #game loop
     while True:
+
+        #close window event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        #Color screen
         pygame.Surface.fill(screen,"#000000")
+        
+        updatable.update(dt)
+        player.draw(screen)
+        
+        #end of frame
         pygame.display.flip()
         dt = (pygame.time.Clock.tick(clk, 60)) / 1000
+
+
+
 
 if __name__ == "__main__":
     main()
